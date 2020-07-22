febc7b3bf867d7270bbeccb16a3fedf119d8c268
Carolin Wagner <ich.caro@gmail.com>
2020-07-22 16:07:22 +0200

    Add navigation JS

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 "use strict";
 
 console.log('it works');
-console.log('hello world');
-console.log('test');
-console.log('hello');
+var homeIcon = document.querySelector('.home-icon');
+var bookmarkIcon = document.querySelector('.bookmark-icon');
+var createIcon = document.querySelector('.create-icon');
+var profileIcon = document.querySelector('.profile-icon');
+var pageIndex = document.querySelector('.page-index');
+var pageBookmark = document.querySelector('.page-bookmark');
+var pageCreate = document.querySelector('.page-create');
+var pageProfile = document.querySelector('.page-profile');
+homeIcon.addEventListener('click', function () {
+  pageIndex.classList.remove('hidden');
+  pageBookmark.classList.add('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.add('hidden');
+});
+bookmarkIcon.addEventListener('click', function () {
+  pageIndex.classList.add('hidden');
+  pageBookmark.classList.remove('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.add('hidden');
+});
+createIcon.addEventListener('click', function () {
+  pageIndex.classList.add('hidden');
+  pageBookmark.classList.add('hidden');
+  pageCreate.classList.remove('hidden');
+  pageProfile.classList.add('hidden');
+});
+profileIcon.addEventListener('click', function () {
+  pageIndex.classList.add('hidden');
+  pageBookmark.classList.add('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.remove('hidden');
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,38 @@
 console.log('it works')
-console.log('hello world')
-console.log('test')
-console.log('hello')
+
+const homeIcon = document.querySelector('.home-icon')
+const bookmarkIcon = document.querySelector('.bookmark-icon')
+const createIcon = document.querySelector('.create-icon')
+const profileIcon = document.querySelector('.profile-icon')
+const pageIndex = document.querySelector('.page-index')
+const pageBookmark = document.querySelector('.page-bookmark')
+const pageCreate = document.querySelector('.page-create')
+const pageProfile = document.querySelector('.page-profile')
+
+homeIcon.addEventListener('click', () => {
+  pageIndex.classList.remove('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+})
+
+bookmarkIcon.addEventListener('click', () => {
+  pageIndex.classList.add('hidden')
+  pageBookmark.classList.remove('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+})
+
+createIcon.addEventListener('click', () => {
+  pageIndex.classList.add('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.remove('hidden')
+  pageProfile.classList.add('hidden')
+})
+
+profileIcon.addEventListener('click', () => {
+  pageIndex.classList.add('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.remove('hidden')
+})
